c6bfc686e1b5786f2dcc26c99d09670ccf9cf086
Nijat2006 <nijatrg@code.edu.az>
2022-09-08 10:12:01 +0400

    first commit

---
+++ Js intro/asset/js/app.js
@@ -0,0 +1,19 @@
+
+var tarix = new Date().getHours();
+var tarix2 = new Date().getHours();
+if(tarix === tarix2){
+    document.body.style.backgroundColor ='blue'
+    document.body.style.color ='red'
+    alert('9:59')
+}
+else{
+    document.body.style.backgroundColor ='blue'
+    document.body.style.color ='red'
+    alert('9:59')
+}
+   
+
+
+
+
+
