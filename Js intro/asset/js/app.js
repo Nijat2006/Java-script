c6a4e1b37da9ac2d651427a7cc28ef47f2d065e9
Nijat2006 <nijatrg@code.edu.az>
2022-09-08 17:08:52 +0400

    first

--- Js intro/asset/js/app.js
+++ Js intro/asset/js/app.js
@@ -1,16 +1,24 @@
 
-var tarix = new Date().getHours();
-var tarix2 = new Date().getHours();
-if(tarix === tarix2){
-    document.body.style.backgroundColor ='blue'
-    document.body.style.color ='red'
-    alert('9:59')
+var hours = new Date().getHours();
+console.log(hours);
+if( hours >=6 && hours <=12) {
+    alert('good morning ....')
+    document.body.style.backgroundImage="url('https://a.travel-assets.com/findyours-php/viewfinder/images/res70/339000/339656-Downtown-Miami.jpg?impolicy=fcrop&w=360&h=224&q=mediumLow')"
 }
-else{
-    document.body.style.backgroundColor ='blue'
-    document.body.style.color ='red'
-    alert('9:59')
+else if(hours>12 && hours <=18){
+    alert('good afternon')
+    document.body.style.backgroundImage="url('https://toplum.tv/images/2020-04/1586169886_trend_baku_city_spring_270415_37.jpg')"
 }
+else if(hours> 18 && tarix <=24){
+    valert('Good night...')
+    document.body.style.backgroundImage="url('https://news24.az/uploads/posts/2019-08/1567060512_baki.jpg')"
+}
+else if(hours> 0 && hours <6) {
+    alert('HI')
+    document.body.style.backgroundImage="url('https://media-cdn.tripadvisor.com/media/photo-s/15/48/ff/98/hotel-grande-italia.jpg')"
+
+}
+
    
 
 
